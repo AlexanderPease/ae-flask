ec9e7087b25d6562c94d2861b98eb31d329d7c69
AlexanderPease <zander@nomadhealth.com>
2018-08-28 10:58:11 -0400

    Working webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+const config = {
+    entry:  __dirname + '/static/js/index.jsx',
+    output: {
+        path: __dirname + '/static/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+};
+
+module: {
+  rules: [
+    {
+      test: /\.jsx?/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }
+  ]
+}
+
+module.exports = config;
