5b6eb7f042227e5f98d950d01e999b6a3e0762bb
AlexanderPease <zander@nomadhealth.com>
2018-08-28 16:24:57 -0400

    Parcel WIP

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,13 @@ const config = {
 module: {
   rules: [
     {
-      test: /\.jsx?/,
+      test: /\.js$/,
       exclude: /node_modules/,
-      use: 'babel-loader'
+      use: 'babel-loader',
+      query: {
+        // presets: ['babel/preset-env','@babel/preset-react']
+        presets: ['env', 'react']
+      }
     }
   ]
 }
